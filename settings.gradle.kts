7c14da5801ee98fa5b166b8160bc51970839c589
mauricio-02 <79987491+mauricio-02@users.noreply.github.com>
2024-09-26 11:20:59 -0600

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "G7S21_DISENOINTERFAZ_PT3_MJGQ"
+include(":app")
